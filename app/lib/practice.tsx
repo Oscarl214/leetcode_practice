f604d8f6511f8ebd3abc84bd4338023ca42f3175
Oscar Leal <oscarleal234@gmail.com>
2024-11-09 17:55:43 -0600

    practiced on plusMinus problem

--- app/lib/practice.tsx
+++ app/lib/practice.tsx
@@ -3,3 +3,31 @@
 //1. Plus Minus
 //2. Mini-Max Sum
 //3. Time Conversion
+
+//1. Plus Minus
+//Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero.
+//Print the decimal value of each fraction on a new line with places after the decimal.
+
+export function plusMinus(arr: any) {
+  let positiveCount = 0;
+  let negativeCount = 0;
+  let zeroCount = 0;
+
+  arr.forEach((num: any) => {
+    if (num > 0) {
+      positiveCount++;
+    } else if (num < 0) {
+      negativeCount++;
+    } else {
+      zeroCount++;
+    }
+  });
+
+  let ArrayTotal = arr.length;
+
+  return [
+    (positiveCount / ArrayTotal).toFixed(6),
+    (negativeCount / ArrayTotal).toFixed(6),
+    (zeroCount / ArrayTotal).toFixed(6),
+  ];
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { plusMinus } from './lib/practice';
+
 export default function Home() {
+  const arr = [3, 4, 0, -5, 6, 10, -4];
+
+  const amount = plusMinus(arr);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-7xl font-bold">LEET CODE!</h1>
+      <div>{amount[2]}</div>
     </div>
   );
 }
